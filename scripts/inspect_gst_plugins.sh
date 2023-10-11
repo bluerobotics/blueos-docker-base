c3708419be896460254235ce950b0850c4dc0035
Dan Rose <danoftheroses@gmail.com>
2023-10-11 02:50:47 +0000

    scripts: Update to detect aarch64
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- scripts/inspect_gst_plugins.sh
+++ scripts/inspect_gst_plugins.sh
@@ -47,7 +47,8 @@ PLUGINS=(
 ARCH=${ARCH:-$(uname -m)}
 GST_OMX_ENABLED=${GST_OMX_ENABLED:-false}
 LIBCAMERA_ENABLED=${LIBCAMERA_ENABLED:-false}
-if [[ $ARCH == arm* ]]; then
+if [[ $ARCH =~ ^(arm|aarch64) ]]; then ARM=true; else ARM=false; fi
+if [[ $ARM == true ]]; then
     RPICAM_ENABLED=${RPICAM_ENABLED:-false}
 
     if [ $RPICAM_ENABLED == true ] && [ -f /dev/vchiq ]; then
