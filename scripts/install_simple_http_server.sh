3f6cc2f01503b93ac611d1b7d7cb3589fe873323
João Antônio Cardoso <joao.maker@gmail.com>
2024-03-08 21:23:12 -0300

    scripts: Update simple_http_server from v0.6.7 to v0.6.9

--- scripts/install_simple_http_server.sh
+++ scripts/install_simple_http_server.sh
@@ -3,7 +3,7 @@
 # exit when any command fails
 set -e
 
-VERSION="v0.6.7"
+VERSION="v0.6.9"
 
 # By default we install armv7
 REMOTE_BINARY_URL="https://github.com/TheWaWaR/simple-http-server/releases/download/${VERSION}/armv7-unknown-linux-musleabihf-simple-http-server"
